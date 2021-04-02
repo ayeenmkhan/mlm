fc08189f1d83537af644f79955c33b8e51fbb403
navnish <navnishlnb@gmail.com>
2021-04-02 16:16:45 +0000

    added config

---
+++ common/config.php
@@ -0,0 +1,41 @@
+<?php
+
+define('OK_LOADME', 1);
+error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
+
+// --- start ---
+
+date_default_timezone_set('UTC');
+//Live server
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'u326449324_immortal');
+// define('DB_PASSWORD', 'Admin@1122!');
+// define('DB_NAME', 'u326449324_immortal');
+// define('DB_TBLPREFIX', 'netw');
+
+//Localserver
+define('DB_HOST', 'localhost');
+define('DB_USER', 'sachk6ni');
+define('DB_PASSWORD', 'Icchatva@7623!');
+define('DB_NAME', 'sachk6ni_mlm_project');
+define('DB_TBLPREFIX', 'netw');
+
+// define('INSTALL_PATH', 'C:\localhost');
+define('INSTALL_PATH', $_SERVER['DOCUMENT_ROOT'].'/mlm');
+define('SURL','https://'.$_SERVER['HTTP_HOST'].'/mlm');
+define('EMAIL', $_SERVER['HTTP_HOST'].'/mlm/');
+define('DECIMAL_POINT', 2);
+
+define('DEFIMG_LOGO', '../assets/image/logo_defaultimage.png');
+define('DEFIMG_PLAN', '../assets/image/plan_defaultimage.jpg');
+define('DEFIMG_FILE', '../assets/image/file_defaultimage.jpg');
+define('DEFIMG_SITE', '../assets/image/site_defaultimage.jpg');
+define('DEFIMG_ADM', '../assets/image/adm_defaultimage.jpg');
+define('DEFIMG_MBR', '../assets/image/mbr_defaultimage.jpg');
+
+define('ADMFOLDER_NAME', 'admin');
+define('MBRFOLDER_NAME', 'member');
+define('UIDFOLDER_NAME', 'id');
+
+//ini_set('log_errors', 'On');
+//ini_set('error_log', 'netw_error.log');
